88a015c8ff417984989c2a26d906c3102dbea4a8
Readerr <e_paunova@yahoo.com>
2015-05-06 12:05:20 -0400

    Final plots

--- plot4.R
+++ plot4.R
@@ -7,21 +7,50 @@ dataset$DateTime <- paste(dataset$Date, dataset$Time)
 dataset$DateTime <- as.POSIXct(strptime(dataset$DateTime, '%d/ %m/ %Y %H:%M:%S' ) )
 data_subset <- dataset[ dataset$DateTime < '2007-02-03 00:00:00', ]
 
+png( filename = 'plot4.png', width = 480, height = 480, bg = NA )
+
 par( mfrow = c(2, 2) )
 
+# 1
 plot( data_subset$Global_active_power ~ data_subset$DateTime,
       type = 'l',
       xlab = '',
       ylab = 'Global Active Power'
 )
-
+# 2
 plot( data_subset$Voltage ~ data_subset$DateTime,
       type = 'l',
       xlab = 'datetime',
       ylab = 'Voltage'
 )
 
+# 3
 plot( data_subset$Sub_metering_1 ~ data_subset$DateTime, 
-      
+      ylab = 'Energy sub metering',
+      xlab = '',
       type='n' )
 
+lines( data_subset$Sub_metering_1 ~ data_subset$DateTime,
+      col = 'black',
+      type = 'l'
+      )
+lines( data_subset$Sub_metering_2 ~ data_subset$DateTime,
+       col = 'red',
+       type = 'l'
+       )
+lines( data_subset$Sub_metering_3 ~ data_subset$DateTime,
+       col = 'blue',
+       type = 'l'
+       )
+legend( 'topright', lty = c(1,1,1), box.col = NA, col = c('black', 'red', 'blue'), legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3') )
+
+
+# 4
+plot( data_subset$Global_reactive_power ~ data_subset$DateTime,
+      type = 'l',
+      col = 'black',
+      ylab = 'Global_reactive_power',
+      xlab = 'datatime'
+      )
+
+dev.off()
